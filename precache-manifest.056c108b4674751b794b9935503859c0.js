db430e380f5c704819008cfe4e63bc4c90306ad8
YODA <dagobah-support@disneystreaming.com>
2020-07-26 23:19:22 -0400

    Updates

---
+++ precache-manifest.056c108b4674751b794b9935503859c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fbef6fe6fde9a7814c3add945531ca",
+    "url": "/react-covid-app/index.html"
+  },
+  {
+    "revision": "8ebe094ff336506ad8be",
+    "url": "/react-covid-app/static/css/main.5a375603.chunk.css"
+  },
+  {
+    "revision": "a419a94cb8cf72f181a9",
+    "url": "/react-covid-app/static/js/2.9ac58f04.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid-app/static/js/2.9ac58f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebe094ff336506ad8be",
+    "url": "/react-covid-app/static/js/main.de79ce6d.chunk.js"
+  },
+  {
+    "revision": "e32b0924acd0a635fc8a",
+    "url": "/react-covid-app/static/js/runtime-main.ab6ab947.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid-app/static/media/image.d7265326.png"
+  }
+]);
